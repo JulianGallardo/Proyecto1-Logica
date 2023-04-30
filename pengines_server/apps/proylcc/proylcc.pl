0d47746f68bf75fc34822435e04e6cc686ecfb8f
Julian Gallardo <juliangallardo26603@gmail.com>
2023-04-30 19:59:53 -0300

    Documentacion casi completa

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -145,96 +145,105 @@ reemplazarPorPotencias([X|Xs], [Y|Res]) :- X=0, generarPotencia2Random(Y), reemp
 
 
 /**
- * 
- * 
+ * filaPos(+Pos,-ResFila)
+ * ResFila es el numero de la fila de la posicion Pos.
  **/
 filaPos(Pos,0):-Pos<5,Pos>=0.
 filaPos(Pos,ResFila):-PosAux is Pos-5,Pos>=0,filaPos(PosAux,ResAux),ResFila is ResAux+1.
 
 /**
- * 
- * 
+ * mismaFila(+Pos1,+Pos2)
+ * Devuelve true si Pos1 y Pos2 son de la misma fila, sino false.
  **/
-calcularFila(Pos1,Fila1,Pos2,Fila2):-filaPos(Pos1,Fila1),filaPos(Pos2,Fila2).
-
-/**
- * 
- * 
- **/
-mismaFila(Pos1,Pos2):-calcularFila(Pos1,Fila1,Pos2,Fila2),Fila1 =:= Fila2.
+mismaFila(Pos1,Pos2):-filaPos(Pos1,Fila1),filaPos(Pos2,Fila2),Fila1 =:= Fila2.
 
 
 /**
- * 
- * 
+ * filaAbajo(+Pos1,+Pos2)
+ * Devuelve true si la Pos1 esta una fila abajo de Pos2, sino false.
  **/
 filaAbajo(Pos1,Pos2) :-filaPos(Pos1,Res1),filaPos(Pos2,Res2),Res2 is Res1+1.
 
 /**
- * 
- * 
+ * filaArriba(+Pos1,+Pos2)
+ * Devuelve true si la Pos1 esta una fila arriba de Pos2, sino false.
  **/
 filaArriba(Pos1,Pos2) :-filaPos(Pos1,Res1),filaPos(Pos2,Res2),Res2 is Res1-1.
 
 /**
- * 
- * 
+ * adyacenteArriba(+Grid,+Elemento,+Pos,+Lista,-ListaProcesada)
+ * ListaProcesada es el resultado de procesar el adyacente de Arriba de la posicion Pos, si el elemento en la posicion del adyacente es igual al elemento Elemento, entonces se buscan los
+ * adyacentes del adyacente. 
  **/
 adyacenteArriba(Grid,Elemento, Pos,Lista,ListaProcesada) :- PosAux is Pos-5, PosAux>=0,not(member(PosAux,Lista)),nth0(PosAux, Grid, Achequear), Elemento=:=Achequear,adyacentes(Grid,Elemento,PosAux,[PosAux|Lista],ListaProcesada).
 adyacenteArriba(_Grid, _Elemento, _Pos,Lista,Lista).
 
 /**
- * 
- * 
+ * adyacenteAbajo(+Grid,+Elemento,+Pos,+Lista,-ListaProcesada)
+ * ListaProcesada es el resultado de procesar el adyacente de Abajo de la posicion Pos, si el elemento en la posicion del adyacente es igual al elemento Elemento, entonces se buscan los
+ * adyacentes del adyacente. 
  **/
 adyacenteAbajo(Grid,Elemento, Pos,Lista,ListaProcesada):-PosAux is Pos+5,PosAux<40,not(member(PosAux,Lista)),nth0(PosAux,Grid,Achequear),Elemento=:=Achequear,adyacentes(Grid,Elemento,PosAux,[PosAux|Lista],ListaProcesada).
 adyacenteAbajo(_Grid, _Elemento, _Pos,Lista,Lista).
 
 /**
- * 
+ * adyacenteIzquierda(+Grid,+Elemento,+Pos,+Lista,-ListaProcesada)
+ * ListaProcesada es el resultado de procesar el adyacente de la Izquierda de la posicion Pos, si el elemento en la posicion del adyacente es igual al elemento Elemento, entonces se buscan los
+ * adyacentes del adyacente. 
  * 
  **/
 adyacenteIzquierda(Grid,Elemento, Pos,Lista,ListaProcesada):-PosAux is Pos-1,PosAux>=0,not(member(PosAux,Lista)),  nth0(PosAux,Grid,Achequear),Elemento=:=Achequear,mismaFila(Pos, PosAux),adyacentes(Grid,Elemento,PosAux,[PosAux|Lista],ListaProcesada).
 adyacenteIzquierda(_Grid, _Elemento, _Pos,Lista,Lista).
 
 /**
- * 
+ * adyacenteDerecha(+Grid,+Elemento,+Pos,+Lista,-ListaProcesada)
+ * ListaProcesada es el resultado de procesar el adyacente de la Derecha de la posicion Pos, si el elemento en la posicion del adyacente es igual al elemento Elemento, entonces se buscan los
+ * adyacentes del adyacente.
  * 
  **/
 adyacenteDerecha(Grid,Elemento, Pos,Lista,ListaProcesada):-PosAux is Pos+1,PosAux<40,not(member(PosAux,Lista)),  nth0(PosAux,Grid,Achequear),Elemento=:=Achequear,mismaFila(Pos, PosAux),adyacentes(Grid,Elemento,PosAux,[PosAux|Lista],ListaProcesada).
 adyacenteDerecha(_Grid, _Elemento, _Pos,Lista,Lista).
 
 /**
- * 
+ * adyacenteIzquierdaAbajo(+Grid,+Elemento,+Pos,+Lista,-ListaProcesada)
+ * ListaProcesada es el resultado de procesar el adyacente inferior izquierdo de la posicion Pos, si el elemento en la posicion del adyacente es igual al elemento Elemento, entonces se buscan los
+ * adyacentes del adyacente.
  * 
  **/
 adyacenteIzquierdaAbajo(Grid,Elemento, Pos,Lista,ListaProcesada) :-PosAux is Pos+4, PosAux<40,not(member(PosAux,Lista)), nth0(PosAux,Grid,Achequear),Elemento=:=Achequear, filaAbajo(Pos,PosAux),adyacentes(Grid,Elemento,PosAux,[PosAux|Lista],ListaProcesada).
 adyacenteIzquierdaAbajo(_Grid, _Elemento, _Pos,Lista,Lista).
 
 /**
- * 
+ * adyacenteIzquierdaAbajo(+Grid,+Elemento,+Pos,+Lista,-ListaProcesada)
+ * ListaProcesada es el resultado de procesar el adyacente superior izquierdo de la posicion Pos, si el elemento en la posicion del adyacente es igual al elemento Elemento, entonces se buscan los
+ * adyacentes del adyacente.
  * 
  **/
 adyacenteIzquierdaArriba(Grid,Elemento, Pos,Lista,ListaProcesada) :- PosAux is Pos-6, PosAux>=0,not(member(PosAux,Lista)), nth0(PosAux,Grid,Achequear),Elemento=:=Achequear, filaArriba(Pos, PosAux),adyacentes(Grid,Elemento,PosAux,[PosAux|Lista],ListaProcesada).
 adyacenteIzquierdaArriba(_Grid, _Elemento, _Pos,Lista,Lista).
 
 /**
- * 
+ * adyacenteIzquierdaAbajo(+Grid,+Elemento,+Pos,+Lista,-ListaProcesada)
+ * ListaProcesada es el resultado de procesar el adyacente inferior derecho de la posicion Pos, si el elemento en la posicion del adyacente es igual al elemento Elemento, entonces se buscan los
+ * adyacentes del adyacente.
  * 
  **/
 adyacenteDerechaAbajo(Grid,Elemento, Pos,Lista,ListaProcesada) :- PosAux is Pos+6,PosAux<40,not(member(PosAux,Lista)),nth0(PosAux,Grid,Achequear),Elemento=:=Achequear, filaAbajo(Pos,PosAux),adyacentes(Grid,Elemento,PosAux,[PosAux|Lista],ListaProcesada).
 adyacenteDerechaAbajo(_Grid, _Elemento, _Pos,Lista,Lista).
 
 /**
- * 
+ * adyacenteIzquierdaAbajo(+Grid,+Elemento,+Pos,+Lista,-ListaProcesada)
+ * ListaProcesada es el resultado de procesar el adyacente superior derecho de la posicion Pos, si el elemento en la posicion del adyacente es igual al elemento Elemento, entonces se buscan los
+ * adyacentes del adyacente.
  * 
  **/
 adyacenteDerechaArriba(Grid,Elemento, Pos,Lista,ListaProcesada) :- PosAux is Pos-4,PosAux>0,not(member(PosAux,Lista)), nth0(PosAux,Grid,Achequear),Elemento=:=Achequear,filaArriba(Pos, PosAux),adyacentes(Grid,Elemento,PosAux,[PosAux|Lista],ListaProcesada).
 adyacenteDerechaArriba(_Grid, _Elemento, _Pos,Lista,Lista).
 
 /**
- * 
- * 
+ * adyacentes(+Grid,+ElemInPos,+Pos,+Lista,+ListaRes8)
+ * ListaRes8 es la lista procesada luego de calcular todos los adyacentes de la posicion Pos y sus adyacentes en la grilla Grid.
+ * Un adyacente se guarda en la lista de adyacentes solo si el elemento en la posicon adyacente es igual al elemento ElemInPos.
  **/
 adyacentes(Grid, ElemInPos, Pos,Lista, ListaRes8) :-
     adyacenteDerechaAbajo(Grid, ElemInPos, Pos,Lista,ListaRes1),adyacenteAbajo(Grid,ElemInPos,Pos,ListaRes1,ListaRes2),adyacenteIzquierdaAbajo(Grid, ElemInPos, Pos, ListaRes2,ListaRes3),
@@ -243,28 +252,28 @@ adyacentes(Grid, ElemInPos, Pos,Lista, ListaRes8) :-
 
 
 /**
- * 
- * 
+ * sacarGrupo(+Grid,+Pos,-Res)
+ * Res es una lista con los elementos de un grupo , empezando por la posicion Pos de la grilla Grid. 
  **/ 
 sacarGrupo(Grid,Pos,Res):-nth0(Pos,Grid,ElemInPos),adyacentes(Grid,ElemInPos,Pos,[],Res).
 
  /**
- * 
- * 
+* estaContenida(+ListaDeListas,+ListaBuscada)
+ * Devuelve true si la ListaBuscada es igual a alguna de las Listas en la ListaDeListas, no importa si los elementos no tiene el mismo orden.
  **/ 
-esta_contenida([ListaCabeza|_SubListas], ListaBuscada) :-mismaLista(ListaCabeza,ListaBuscada).
-esta_contenida([_ListaCabeza|SubListas], ListaBuscada) :-esta_contenida(SubListas,ListaBuscada).
+estaContenida([ListaCabeza|_SubListas], ListaBuscada) :-mismaLista(ListaCabeza,ListaBuscada).
+estaContenida([_ListaCabeza|SubListas], ListaBuscada) :-estaContenida(SubListas,ListaBuscada).
     
 /**
- * 
- * 
+ * mismaLista(+ListaAComprobar,+Lista)
+ * Devuelve true si ambas listas tienen los mismos elementos.
  **/ 
 mismaLista(ListaAComprobar,[X]):-member(X,ListaAComprobar).
 mismaLista(ListaAComprobar,[X|Xs]):-member(X,ListaAComprobar),mismaLista(ListaAComprobar,Xs).
 
 /**
  * 
- * 
+ * VER SI NO SE PUEDE REEMPLAZAR POR EL HECHO ANTES EN EL JOIN YA QUE HACEN LO MISMO PERO UNO CON PATH Y EL OTRO CON INDICES DE LA GRILLA
  **/
 
 calcularSuma(Grid, [X], Res) :- buscarElementoI(Grid, X, Res).
@@ -272,18 +281,20 @@ calcularSuma(Grid, [X|Xs], Res) :- calcularSuma(Grid, Xs, ResAux),buscarElemento
 
 
 /**
- * 
- * 
+ * sacarGruposGrilla(+Grid,+ListaGrupos,-ListaDeGruposFinal)
+ * ListaDeGruposFinal es una lista la cual contiene todos los grupos de la grilla.
+ * ListaGrupos es una lista la cual va guardando los grupos armados, para no tener duplicados.
  **/ 
-sacarGruposGrilla(Grid, 39, ListaGrupos, [ResAux|ListaGrupos]) :- sacarGrupo(Grid, 39, ResAux),length(ResAux,Long),Long>1,not(esta_contenida(ListaGrupos,ResAux)).
+sacarGruposGrilla(Grid, 39, ListaGrupos, [ResAux|ListaGrupos]) :- sacarGrupo(Grid, 39, ResAux),length(ResAux,Long),Long>1,not(estaContenida(ListaGrupos,ResAux)).
 sacarGruposGrilla(_Grid, 39, ListaGrupos, ListaGrupos).
-sacarGruposGrilla(Grid, Posicion, ListaGrupos, Res) :- sacarGrupo(Grid, Posicion, ResAux),length(ResAux,Long),Long>1, not(esta_contenida(ListaGrupos,ResAux)), Posicion2 is Posicion+1, sacarGruposGrilla(Grid, Posicion2,[ResAux|ListaGrupos],Res).
-sacarGruposGrilla(Grid, Posicion, ListaGrupos, Res) :- Posicion2 is Posicion+1, sacarGruposGrilla(Grid, Posicion2, ListaGrupos, Res).
+sacarGruposGrilla(Grid, Posicion, ListaGrupos, ListaDeGruposFinal) :- sacarGrupo(Grid, Posicion, ResAux),length(ResAux,Long),Long>1, not(estaContenida(ListaGrupos,ResAux)), Posicion2 is Posicion+1, sacarGruposGrilla(Grid, Posicion2,[ResAux|ListaGrupos],ListaDeGruposFinal).
+sacarGruposGrilla(Grid, Posicion, ListaGrupos, ListaDeGruposFinal) :- Posicion2 is Posicion+1, sacarGruposGrilla(Grid, Posicion2, ListaGrupos, ListaDeGruposFinal).
 
 
 /**
- * 
- * 
+ * reemplazarElemetosGrupo(+Grid,+ListaGrupo,+Max,+ProximaPotencia,-GridRes)
+ * GridRes es una grilla en la cual los elementos de ListaGrupo son reemplazados por 0, salvo el mayor Max, que es reemplazado por ProxPotencia, ya que seria el elemento de mas 
+ * abajo a la derecha. ProxPotencia es la proxima potencia de 2, de la suma de los elementos del grupo.
  **/     
 reemplazarElementosGrupo(Grid,[X],Max,ProxPotencia,GridRes):-X=Max,reemplazarElementoI(Grid,X,ProxPotencia,GridRes).     
 reemplazarElementosGrupo(Grid,[X],Max,_ProxPotencia,GridRes):-X\=Max,reemplazarElementoI(Grid,X,0,GridRes). 
@@ -291,17 +302,24 @@ reemplazarElementosGrupo(Grid,[X|Xs],Max,ProxPotencia,GridRes):-X=Max,reemplazar
 reemplazarElementosGrupo(Grid,[X|Xs],Max,ProxPotencia,GridRes):-X\=Max,reemplazarElementoI(Grid,X,0,GridAux),reemplazarElementosGrupo(GridAux,Xs,Max,ProxPotencia,GridRes).    
 
 /**
- * 
- * 
+ * procesarEliminacionGrupos(+Grid,+ListaDeGrupos,-GridRes)
+ * GridRes es la grilla resultante de procesar todos los Grupos en ListaDeGrupos. 
  **/ 
 procesarEliminacionGrupos(Grid,[ListaCabeza],GridRes):-max_list(ListaCabeza,Maximo),calcularSuma(Grid,ListaCabeza,ResultadoSuma),calcularProximaPotencia(ResultadoSuma, 2, Res),reemplazarElementosGrupo(Grid,ListaCabeza,Maximo,Res,GridRes).
 procesarEliminacionGrupos(Grid,[ListaCabeza|RestoListas],GridRes):-max_list(ListaCabeza,Maximo),calcularSuma(Grid,ListaCabeza,ResultadoSuma),calcularProximaPotencia(ResultadoSuma, 2, Res),reemplazarElementosGrupo(Grid,ListaCabeza,Maximo,Res,GridAux),procesarEliminacionGrupos(GridAux,RestoListas,GridRes).
 
 /**
- * 
+ * borrarGrupos(+Grid,-GridRes)
+ * GridRes es la grilla con todos los grupos procesados para el ColapsarIguales.
+ * Primero se sacan todos los grupos de la grilla y despues se procesan.
  * 
  **/ 
 borrarGrupos(Grid,GridRes):-sacarGruposGrilla(Grid,0,[],ListaGrupos),procesarEliminacionGrupos(Grid,ListaGrupos,GridRes).
 
 
+/**
+ * powerUp(+Grid,-RGrid)
+ * RGrid es una lista con los pasos que hace la grilla para aplicar el powerUp
+ * El numero 0 en las celdas reperesenta una celda vacia.
+ **/ 
 powerUp(Grid,RGrids):-borrarGrupos(Grid,GridA),gravedad(GridA,GridB),reemplazarPorPotencias(GridB,GridC),RGrids = [GridA,GridB,GridC].
